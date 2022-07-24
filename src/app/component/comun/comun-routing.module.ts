4ea2a511ecb0f6aa6cf974d1f365c41084777ba6
Alejandro Diaz <francisco.alejandro.prado.diaz@gmail.com>
2022-07-24 14:00:46 -0400

    Se genera redireccionamiento a home

--- src/app/component/comun/comun-routing.module.ts
+++ src/app/component/comun/comun-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PaginanoencontradaComponent } from './paginanoencontrada/paginanoencontrada.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [{ path: '**', component: PaginanoencontradaComponent }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ComunRoutingModule { }
+export class ComunRoutingModule {}
